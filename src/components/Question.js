207f10d1a29c211ad812dd09972f363678240f3e
hellenwamaitha <hellenwama254@gmail.com>
2023-07-22 00:23:39 +0300

    effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,6 +4,22 @@ function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    // Only set a timeout if there is time remaining.
+    if (timeRemaining > 0) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1);
+      }, 1000);
+
+      // Cleanup function to clear the timeout
+      return () => clearTimeout(timerId);
+    } else {
+      // If timeRemaining is 0, reset the timer and invoke the onAnswered callback
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
